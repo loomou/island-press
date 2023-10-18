75bd121e805b24a9f5f20dac6f3fbe4feb7bc65d
loomou <273226981@qq.com>
2023-10-18 22:09:33 +0800

    feat: 服务端构建

--- src/node/cli.ts
+++ src/node/cli.ts
@@ -1,6 +1,7 @@
 import cac from 'cac';
 import { createDevServer } from './dev';
-import * as path from 'path';
+import { resolve } from 'path';
+import { build } from './build';
 
 
 const version = require('../../package.json').version;
@@ -11,7 +12,7 @@ cli
   .command('[root]', 'start dev server')
   .alias('dev')
   .action(async (root: string) => {
-    root = root ? path.resolve(root) : process.cwd();
+    root = root ? resolve(root) : process.cwd();
     const server = await createDevServer(root);
     await server.listen();
     server.printUrls();
@@ -20,7 +21,12 @@ cli
 cli
   .command('build [root]', 'build for production')
   .action(async (root: string) => {
-    console.log('build', root);
+    try {
+      root = resolve(root);
+      await build(root);
+    } catch (e) {
+      console.log(e);
+    }
   });
 
 cli.parse();
