ad0ca5fcb39f9f1c957fe998a563d0ecf1ccdc01
loomou <273226981@qq.com>
2023-10-18 22:54:20 +0800

    feat: 通用配置文件解析

---
+++ src/node/config.ts
@@ -0,0 +1,48 @@
+import { resolve } from 'path';
+import fs from 'fs-extra';
+import { loadConfigFromFile } from 'vite';
+import { UserConfig } from '../shared/types/index';
+
+type RawConfig =
+  | UserConfig
+  | Promise<UserConfig>
+  | (() => UserConfig | Promise<UserConfig>);
+
+export async function resolveConfig(
+  root: string,
+  command: 'serve' | 'build',
+  mode: 'development' | 'production'
+) {
+  const configPath = getUserConfigPath(root);
+  const result = await loadConfigFromFile(
+    {
+      command,
+      mode
+    },
+    configPath,
+    root
+  );
+
+  if (result) {
+    const { config: rawConfig = {} as RawConfig } = result;
+    const userConfig = await (typeof rawConfig === 'function'
+      ? rawConfig()
+      : rawConfig);
+    return [configPath, userConfig] as const;
+  } else {
+    return [configPath, {} as UserConfig] as const;
+  }
+}
+
+function getUserConfigPath(root: string) {
+  try {
+    const supportConfigFiles = ['config.ts', 'config.js'];
+    const configPath = supportConfigFiles
+      .map((file) => resolve(root, file))
+      .find(fs.pathExistsSync);
+    return configPath;
+  } catch (e) {
+    console.error(`Failed to load user config: ${e}`);
+    throw e;
+  }
+}
