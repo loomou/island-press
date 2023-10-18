f27f7587ff2aefc1dd1b9c61ed42daa91b678463
loomou <273226981@qq.com>
2023-10-18 21:32:20 +0800

    feat: 入口HTML处理

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,7 +1,11 @@
-import { createServer as createViteDevServer } from "vite";
+import { createServer as createViteDevServer } from 'vite';
+import { pluginIndexHtml } from './plugin-island/indexHtml';
 
 export async function createDevServer(root = process.cwd()) {
   return createViteDevServer({
     root,
+    plugins: [
+      pluginIndexHtml()
+    ]
   });
 }
