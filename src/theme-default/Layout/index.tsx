39ca758cc6105819f5477a7e6209d6d0cebdcc72
loomou <273226981@qq.com>
2023-10-19 18:44:12 +0800

    feat: 根据数据自动组装首页内容

--- src/theme-default/Layout/index.tsx
+++ src/theme-default/Layout/index.tsx
@@ -3,13 +3,14 @@ import '../styles/base.css';
 import '../styles/vars.css';
 import { usePageData } from '@runtime';
 import { Nav } from '../components/Nav';
+import { HomeLayout } from './HomeLayout';
 
 export function Layout() {
   const pageData = usePageData();
   const { pageType } = pageData;
   const getContent = () => {
     if (pageType === 'home') {
-      return <div>Home 页面</div>;
+      return <HomeLayout />;
     } else if (pageType === 'doc') {
       return <div>正文页面</div>;
     } else {
