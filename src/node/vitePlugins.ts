2a671959755154dcaadfe7494db65ede2995fc47
loomou <273226981@qq.com>
2023-10-19 16:42:04 +0800

    feat: unocss

--- src/node/vitePlugins.ts
+++ src/node/vitePlugins.ts
@@ -4,6 +4,8 @@ import { pluginConfig } from './plugin-island/config';
 import { pluginRoutes } from './plugin-routes';
 import { SiteConfig } from 'shared/types';
 import { createPluginMdx } from './plugin-mdx';
+import pluginUnocss from 'unocss/vite';
+import unocssOptions from './unocssOptions';
 
 export async function createVitePlugins(
   config: SiteConfig,
@@ -13,6 +15,7 @@ export async function createVitePlugins(
 ) {
   return [
     await createPluginMdx(mdxHMR),
+    pluginUnocss(unocssOptions),
     pluginIndexHtml(),
     pluginReact({
       jsxRuntime: 'automatic',
--- src/theme-default/Layout/index.tsx
+++ src/theme-default/Layout/index.tsx
@@ -1,9 +1,12 @@
-import { Content } from '../../runtime/Content';
+import 'uno.css';
+import { Content } from '../../runtime';
 
 export function Layout() {
   return (
     <div>
-      <h1>Common Content</h1>
+      <h1 p="2" m="4">
+        Common Content
+      </h1>
       <h1>Doc Content</h1>
       <Content />
     </div>
