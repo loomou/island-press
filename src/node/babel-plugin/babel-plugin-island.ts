3f757645f97b32fda6eb9cd1027142737871d570
loomou <273226981@qq.com>
2023-10-24 23:48:09 +0800

    fix: 解决生产下dev路径错误

--- src/node/babel-plugin/babel-plugin-island.ts
+++ src/node/babel-plugin/babel-plugin-island.ts
@@ -5,34 +5,37 @@ import { normalizePath } from 'vite';
 import type { Visitor } from '@babel/traverse';
 import type { PluginPass } from '@babel/core';
 
+const ID = '__island';
+
 export default declare((api) => {
   api.assertVersion(7);
 
   const visitor: Visitor<PluginPass> = {
     JSXOpeningElement(path, state) {
       const name = path.node.name;
-      let bindingName = '';
-      if (name.type === 'JSXIdentifier') {
+      let bindingName: string;
+      if (t.isJSXIdentifier(name)) {
         bindingName = name.name;
-      } else if (name.type === 'JSXMemberExpression') {
+      } else if (t.isJSXMemberExpression(name)) {
         let object = name.object;
         while (t.isJSXMemberExpression(object)) {
           object = object.object;
         }
         bindingName = object.name;
+      } else if (t.isJSXNamespacedName(name)) {
+        bindingName = name.namespace.name;
       } else {
         return;
       }
 
       const binding = path.scope.getBinding(bindingName);
-
       if (binding?.path.parent.type === 'ImportDeclaration') {
-        const source = binding.path.parent.source;
+        const source = (binding?.path.parent as t.ImportDeclaration).source;
         const attributes = (path.container as t.JSXElement).openingElement
           .attributes;
         for (let i = 0; i < attributes.length; i++) {
           const name = (attributes[i] as t.JSXAttribute).name;
-          if (name?.name === '__island') {
+          if (name?.name === ID) {
             (attributes[i] as t.JSXAttribute).value = t.stringLiteral(
               `${source.value}${MASK_SPLITTER}${normalizePath(
                 state.filename || ''
@@ -45,7 +48,7 @@ export default declare((api) => {
   };
 
   return {
-    name: 'transform-jsx-island',
+    name: 'transform-react-jsx-island',
     visitor
   };
 });
